e6309b43b03e79e33a6ab693695aefd8a52e63fd
[Muhammed Machrouh] <[th3justhacker@gmail.com]>
2023-03-03 10:05:30 +0100

    Prepare app for deployment

--- healtyai_backend/settings.py
+++ healtyai_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-75!25^i&834#ooc6zooweecht04#1$8b=(3o*ghmvb%n%ekvme
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 CACHES = {
     'default': {
